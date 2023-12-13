c997469458db08f05daffbfcb8651a123016cd63
zyf <18362966889@163.com>
2023-12-13 14:38:20 +0800

    advanced lifetime

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -141,4 +141,34 @@ mod tests {
             assert_eq!(config.b, "B".to_string());
         }
     }
+
+    #[derive(Debug, PartialEq)]
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    impl Point {
+        fn move_to(&mut self, x: i32, y: i32) {
+            self.x = x;
+            self.y = y;
+        }
+    }
+
+    #[test]
+    fn test_reborrow() {
+        let mut p = Point { x: 0, y: 0 };
+        let r = &mut p;
+        // reborrow! 此时对`r`的再借用（不可变借用）不会导致跟上面的可变借用冲突
+        let rr: &Point = &*r;
+
+        // 再借用`rr`最后一次使用发生在这里，在它的生命周期中，我们并没有使用原来的借用`r`，因此不会报错
+        println!("{:?}", rr);
+        assert_eq!(*rr, Point { x: 0, y: 0 });
+
+        // 再借用结束后，才去使用原来的借用`r`
+        r.move_to(10, 10);
+        println!("{:?}", r);
+        assert_eq!(*r, Point { x: 10, y: 10 });
+    }
 }
